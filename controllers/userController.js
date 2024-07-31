cb70fdf67b9e7701078ab95536a67f2d755eba93
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-07-31 12:28:42 +0300

    User Registration & Password Hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,44 @@
 const asyncHandler = require("express-async-handler")
+const argon2 = require("argon2");
+const User = require("../models/userModel");
+
 
 const registerUser = asyncHandler(
     async (req, res) => {
+        const {username, email, password} = req.body;
+        if(!username || !email || !password){
+            res.status(400)
+            throw new Error("All fields are mandatory!")
+        }
+        const userAvailable = await User.findOne({email})
+        if(userAvailable){
+            res.status(400)
+            throw new Error("User already registered!")
+        }
+
+        const hashPassword = await argon2.hash(password, {
+            type: argon2.argon2id,        
+            memoryCost: 2 ** 16,         
+            timeCost: 3,                  
+            parallelism: 1
+        })
+
+        console.log("Hashed Password: ", hashPassword)
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashPassword,
+        })
+        res.json(`User created ${user}`)
+
+        if(user) {
+            res.status(201).json({_id: user.id, email: user.email})
+        }
+        else{
+            res.status(400)
+            throw new Error("User data is not valid")
+        }
         res.json({message: "Register the user"})
     })
 
